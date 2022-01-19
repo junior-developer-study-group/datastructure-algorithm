ee3627aa396af9ba91cf2e85ea63906349effdd3
HojinAn <asdv94@naver.com>
2022-01-19 23:50:43 +0900

    220116 solution review

--- hojin/220116_Patterns/class_section5/challenges/findLongestSubstring.js
+++ hojin/220116_Patterns/class_section5/challenges/findLongestSubstring.js
@@ -31,19 +31,20 @@ function findLongestSubstring(str) {
 /////////////////////////////////////////
 
 function findLongestSubstringSol(str) {
-  let longest = 0;
-  let seen = {};
-  let start = 0;
- 
+  let longest = 0; // 가장 긴 길이값을 저장할 longest
+  let seen = {}; // 본 알파벳을 저장할 object
+  let start = 0; // substring의 시작
+
   for (let i = 0; i < str.length; i++) {
     let char = str[i];
     if (seen[char]) {
-      start = Math.max(start, seen[char]);
+      // i index에 해당하는 char가 seen에 있으면
+      start = Math.max(start, seen[char]); // start 값 갱신 즉, substring 시작을 다시하는 것
     }
-    // index - beginning of substring + 1 (to include current in count)
-    longest = Math.max(longest, i - start + 1);
-    // store the index of the next char so as to not double count
-    seen[char] = i + 1;
+    longest = Math.max(longest, i - start); // 매 index마다 즉, 매 substring 길이마다 기존 longest와 비교
+    seen[char] = i; // 해당 char의 value로 index 저장
   }
   return longest;
 }
+
+console.log(findLongestSubstringSol("thisisawsome"));
