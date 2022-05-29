8fb946a5c8cf6b74ca7555b1c3d57832ab1bf217
HojinAn <asdv94@naver.com>
2022-05-29 11:57:03 +0900

    220522 BOJ 1715 카드 정렬하기

---
+++ hojin/220521_Binary_Heaps/BOJ1715_카드정렬하기.js
@@ -0,0 +1,103 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = []; // input 배열 선언
+
+rl.on("line", function (line) {
+  input.push(line); // 입력받는 각 줄의 값을 input 배열에 저장
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+function solution(input) {
+  //1 ≤ N ≤ 100,000
+  const n = Number(input[0]);
+  let pq = new PriorityQueue();
+  for (let i = 1; i <= n; ++i) {
+    let no = Number(input[i]);
+    pq.enqueue(no, no);
+  }
+  let answer = 0;
+  while (pq.values.length > 1) {
+    let sum = 0;
+    sum += pq.dequeue().val;
+    sum += pq.dequeue().val;
+    answer += sum;
+    pq.enqueue(sum, sum);
+  }
+  console.log(answer);
+}
